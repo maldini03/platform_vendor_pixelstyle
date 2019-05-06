f6508351f39ec881518b2b05ceb597d572ac76fb
Mohd Faraz <androiabledroid@gmail.com>
2019-05-06 10:29:00 +0000

    Added Support for dark Animations
     * Some AMOLED devices Looks awesome with this dark bootanimation
    
     * Using zips from https://androidfilehost.com/?w=profile&uid=673791459329056535
    
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- config.mk
+++ config.mk
@@ -36,9 +36,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Bootanimation
 ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
-     PRODUCT_COPY_FILES += vendor/pixelstyle/media/bootanimation_720.zip:system/media/bootanimation.zip
+     ifeq ($(TARGET_BOOT_ANIMATION_RES_DARK),true)
+          PRODUCT_COPY_FILES += vendor/pixelstyle/media/bootanimation_dark_720.zip:system/media/bootanimation.zip
+     else
+          PRODUCT_COPY_FILES += vendor/pixelstyle/media/bootanimation_720.zip:system/media/bootanimation.zip
+     endif
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
-     PRODUCT_COPY_FILES += vendor/pixelstyle/media/bootanimation_1080.zip:system/media/bootanimation.zip
+     ifeq ($(TARGET_BOOT_ANIMATION_RES_DARK),true)
+          PRODUCT_COPY_FILES += vendor/pixelstyle/media/bootanimation_dark_1080.zip:system/media/bootanimation.zip
+     else
+          PRODUCT_COPY_FILES += vendor/pixelstyle/media/bootanimation_1080.zip:system/media/bootanimation.zip
+     endif
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
      PRODUCT_COPY_FILES += vendor/pixelstyle/media/bootanimation_1440.zip:system/media/bootanimation.zip
 else
